0d8de3b7b3f21a133153f55de75186dcb97dbbd6
lisc7136 <lisc7136@bht-berlin.de>
2026-06-25 12:24:36 +0200

    Bewertung von Bewertungen added

--- Movie/urls.py
+++ Movie/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('list', views.MovieListView.as_view(), name='movie_list'),
     path('detail/<int:pk>', views.MovieDetailView.as_view(), name='movie_detail'),
+    path('rating/<int:rating_pk>/vote/<int:vote_value>/', views.vote_rating, name='vote_rating'),
+
 ]
--- Movie/views.py
+++ Movie/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, DetailView, ListView
-from .models import Movie, Rating
+from .models import Movie, Rating, RatingFeedback
 from .forms import RatingForm
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
 # Create your views here.
 class MovieListView(ListView):
     model = Movie
@@ -28,11 +31,35 @@ class MovieDetailView(LoginRequiredMixin, DetailView):
         # Prüfen, ob der User diesen Movie schon bewertet hat
         if Rating.objects.filter(user=request.user, movie=self.object).exists():
             messages.error(request, 'Du hast diesen Movie bereits bewertet.')
-            return redirect('movie-detail', pk=self.object.pk)
+            return redirect('movie_detail', pk=self.object.pk)
 
         if form.is_valid():
             rating = form.save(commit=False)
             rating.user = request.user
             rating.movie = self.object
             rating.save()
-            return redirect('movie-detail', pk=self.object.pk)
+
+            return redirect('movie_detail', pk=self.object.pk)
+
+@login_required
+@require_POST
+def vote_rating(request, rating_pk, vote_value):
+    rating = get_object_or_404(Rating, pk=rating_pk)
+
+    # Nutzer kann eigene Bewertung nicht voten
+    if rating.user == request.user:
+        messages.error(request, 'Du kannst deine eigene Bewertung nicht bewerten.')
+        return redirect('movie_detail', pk=rating.movie.pk)
+
+    feedback, created = RatingFeedback.objects.get_or_create(
+        user=request.user,
+        rating=rating,
+        defaults={'vote': vote_value}
+    )
+
+    if not created:
+        # Vote schon vorhanden -> aktualisieren (z.B. von Upvote auf Downvote wechseln)
+        feedback.vote = vote_value
+        feedback.save()
+
+    return redirect('movie_detail', pk=rating.movie.pk)
