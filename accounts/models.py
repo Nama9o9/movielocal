f5d556597c3ca580552e457472384d0afc54a153
lisc7136 <lisc7136@bht-berlin.de>
2026-06-28 11:07:36 +0200

    added role to MyUser

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class MyUser(AbstractUser):
+    ROLE_CHOICES = ([
+        ('C', 'Customer'),
+        ('S', 'Staff')
+    ])
     profile_pic = models.ImageField(upload_to='profile_pics', null=True, blank=True)
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES, default='C')
