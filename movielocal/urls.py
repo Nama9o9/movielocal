beb3f2b98169ace39daaedba0564fcc1547402fe
lisc7136 <lisc7136@bht-berlin.de>
2026-07-08 10:32:09 +0200

    deleted home.html

--- movielocal/urls.py
+++ movielocal/urls.py
@@ -24,7 +24,7 @@ from django.views.generic import TemplateView, RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('home/', RedirectView.as_view(url="/movie/list", permanent=False), name='movie_list'),
     path('shoppingcart/', include('shoppingcart.urls')),
     path('movie/', include('Movie.urls')),
 path('shop/', include('Shop.urls')),
